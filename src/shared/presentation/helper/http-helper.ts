76cdf0f46f74da85ed6bb79b907b2060e68926bf
Raphael Castro <rafaphcastro@gmail.com>
2021-09-28 00:14:58 -0300

    feat: add http status codes utils and refactor to use it

--- src/shared/presentation/helper/http-helper.ts
+++ src/shared/presentation/helper/http-helper.ts
@@ -1,12 +1,20 @@
 import { HttpResponse } from "@app/shared/presentation/protocol/http";
 import { ServerError } from "@app/shared/presentation/error/server";
+import { HttpStatusCodes } from "@app/shared/utils/http-status-codes";
 
 export const badRequest = (error: Error): HttpResponse<Error> => ({
-    statusCode: 400,
+    statusCode: HttpStatusCodes.BAD_REQUEST,
     body: error,
 });
 
 export const serverError = (): HttpResponse<Error> => ({
-    statusCode: 500,
+    statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
     body: new ServerError(),
 });
+
+export function ok<Body>(body: Body): HttpResponse<Body> {
+    return {
+        statusCode: HttpStatusCodes.OK,
+        body
+    };
+}
