75235f05c0f6c06dccbef47daf51c68db68f0948
Raphael Castro <rafaphcastro@gmail.com>
2021-09-27 23:05:08 -0300

    feat: ensure sign up controller returns bad request if invalid email is provided

--- src/authentication/presentation/controller/sign-up/index.ts
+++ src/authentication/presentation/controller/sign-up/index.ts
@@ -2,8 +2,13 @@ import { HttpRequest, HttpResponse } from "@app/shared/presentation/protocol/htt
 import { MissingParameterError } from "@app/shared/presentation/error/missing-parameter";
 import { badRequest, serverError } from "@app/shared/presentation/helper/http-helper";
 import { Controller } from "@app/shared/presentation/protocol/controller";
+import { EmailValidator } from "@app/authentication/presentation/protocol/email-validator";
+import { InvalidParameterError } from "@app/shared/presentation/error/invalid-parameter";
 
 export class SignUpController implements Controller {
+    public constructor(private readonly emailValidator: EmailValidator) {
+    }
+
     public async handle(request: HttpRequest<SignUpController.RequestBody>): Promise<HttpResponse<SignUpController.ResponseBody>> {
         if (request.body === undefined) {
             return serverError();
@@ -22,13 +27,17 @@ export class SignUpController implements Controller {
             }
         }
 
+        if (!this.emailValidator.isValid(request.body.email as string)) {
+            return badRequest(new InvalidParameterError("email"));
+        }
+
         return {
             statusCode: 200,
             body: {
                 name: "any_name",
                 email: "any_email@email.com",
-                password: "any_password"
-            }
+                password: "any_password",
+            },
         };
     }
 }
