03078f549836b32c81a87d06616c6788ed86a49f
Raphael Castro <rafaphcastro@gmail.com>
2021-09-28 22:49:55 -0300

    test: ensure database add account calls AddAccountRepository with correct values

--- test/authentication/data/use-case/database-add-account.spec.ts
+++ test/authentication/data/use-case/database-add-account.spec.ts
@@ -3,16 +3,30 @@ import sinon from "sinon";
 import { DatabaseAddAccount } from "@app/authentication/data/use-case/database-add-account";
 import { Encrypter } from "@app/authentication/data/protocol/encrypter";
 import { AddAccount } from "@app/authentication/domain/use-case/add-account";
+import { AddAccountRepository } from "@app/authentication/data/protocol/add-account-repository";
 
 const makeEncrypter = (): Encrypter => {
     class EncryterStub implements Encrypter {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         public async encrypt(_value: string): Promise<string> {
-            return "encrypted_value";
+            return faker.internet.password();
         }
     }
 
     return new EncryterStub();
+};
+
+const makeAddAccountRepository = (): AddAccountRepository => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
+        public async execute(input: AddAccountRepository.Input): Promise<AddAccountRepository.Output> {
+            return {
+                ...input,
+                id: faker.datatype.uuid(),
+            };
+        }
+    }
+
+    return new AddAccountRepositoryStub();
 };
 
 const makeInput = (input: Partial<AddAccount.Input> = {}): AddAccount.Input => ({
@@ -23,21 +37,24 @@ const makeInput = (input: Partial<AddAccount.Input> = {}): AddAccount.Input => (
 });
 
 const makeSut = (): {
-    encrypter: Encrypter,
+    encrypterStub: Encrypter,
     sut: DatabaseAddAccount
+    addAccountRepositoryStub: AddAccountRepository,
 } => {
-    const encrypter = makeEncrypter();
+    const encrypterStub = makeEncrypter();
+    const addAccountRepositoryStub = makeAddAccountRepository();
     return {
-        encrypter,
-        sut: new DatabaseAddAccount(encrypter),
+        encrypterStub,
+        addAccountRepositoryStub,
+        sut: new DatabaseAddAccount(encrypterStub, addAccountRepositoryStub),
     };
 };
 
 describe("DatabaseAddAccount", () => {
     it("Should call Encrypter with correct password", async () => {
-        const { sut, encrypter } = makeSut();
+        const { sut, encrypterStub } = makeSut();
         const input = makeInput();
-        const encryptStub = sinon.stub(encrypter, "encrypt");
+        const encryptStub = sinon.spy(encrypterStub, "encrypt");
 
         await sut.execute(input);
 
@@ -45,10 +62,27 @@ describe("DatabaseAddAccount", () => {
     });
 
     it("Should throw if Encrypter throws", async () => {
-        const { sut, encrypter } = makeSut();
+        const { sut, encrypterStub } = makeSut();
         const input = makeInput();
-        sinon.stub(encrypter, "encrypt").rejects();
+        sinon.stub(encrypterStub, "encrypt").rejects();
 
         await expect(sut.execute(input)).to.be.eventually.rejected;
+    });
+
+    it("Should call AddAccountRepository with correct values", async () => {
+        const { sut, encrypterStub, addAccountRepositoryStub } = makeSut();
+        const input = makeInput();
+        const encryptedPassword = faker.internet.password();
+
+        sinon.stub(encrypterStub, "encrypt").resolves(encryptedPassword);
+        const addSpy = sinon.spy(addAccountRepositoryStub, "execute");
+
+        await sut.execute(input);
+
+        sinon.assert.calledOnceWithExactly(addSpy, {
+            name: input.name,
+            email: input.email,
+            password: encryptedPassword,
+        });
     });
 });
