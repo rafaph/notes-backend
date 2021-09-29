2736860bc2fd26bfa9b4e3b478158c7123b00435
Raphael Castro <rafaphcastro@gmail.com>
2021-09-29 00:04:24 -0300

    refactor: move sut creation to a factory helper method

--- test/authentication/infrastructure/hashing/encrypter-argon2-adapter.spec.ts
+++ test/authentication/infrastructure/hashing/encrypter-argon2-adapter.spec.ts
@@ -1,9 +1,12 @@
 import argon2 from "argon2";
-import { EncrypterArgon2Adapter } from "@app/authentication/infrastructure/hashing/encrypter-argon2-adapter";
 import sinon from "sinon";
 import faker from "faker";
+import { EncrypterArgon2Adapter } from "@app/authentication/infrastructure/hashing/encrypter-argon2-adapter";
 
 const FAKE_ENCRYPTED_VALUE = faker.internet.password();
+const ARGON2_TYPE = argon2.argon2id;
+
+const makeSut = (): EncrypterArgon2Adapter => new EncrypterArgon2Adapter(ARGON2_TYPE);
 
 describe("EncrypterArgon2Adapter", () => {
     let hashStub: sinon.SinonStub<[
@@ -20,16 +23,16 @@ describe("EncrypterArgon2Adapter", () => {
     });
 
     it("Should call argon2 with correct values", async () => {
-        const type = argon2.argon2id;
-        const sut = new EncrypterArgon2Adapter(type);
+        const sut = makeSut();
         const password = faker.internet.password();
+
         await sut.encrypt(password);
 
-        sinon.assert.calledOnceWithExactly(hashStub, password, { type });
+        sinon.assert.calledOnceWithExactly(hashStub, password, { type: ARGON2_TYPE });
     });
 
     it("Should return a hash on success", async () => {
-        const sut = new EncrypterArgon2Adapter();
+        const sut = makeSut();
         await expect(
             sut.encrypt(faker.internet.password()),
         ).to.eventually.be.equal(FAKE_ENCRYPTED_VALUE);
