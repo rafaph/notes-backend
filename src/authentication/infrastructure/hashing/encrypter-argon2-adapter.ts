557329476602da1077d73986408402bdeb990df5
Raphael Castro <rafaphcastro@gmail.com>
2021-09-29 00:01:33 -0300

    feat: ensure EncrypterArgon2Adapter returns a hash on success

--- src/authentication/infrastructure/hashing/encrypter-argon2-adapter.ts
+++ src/authentication/infrastructure/hashing/encrypter-argon2-adapter.ts
@@ -2,7 +2,7 @@ import { Encrypter } from "@app/authentication/data/protocol/encrypter";
 import argon2 from "argon2";
 
 export class EncrypterArgon2Adapter implements Encrypter {
-    public constructor(private readonly type: argon2.Options["type"]) {
+    public constructor(private readonly type?: argon2.Options["type"]) {
         this.type = type;
     }
 
