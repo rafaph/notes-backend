563c4782f19aeb90c972846c9565ba283ade7978
Raphael Castro <rafaphcastro@gmail.com>
2021-09-30 22:46:02 -0300

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { bodyParser } from "@app/main/middlewares/body-parser";
+import { cors } from "@app/main/middlewares/cors";
 
 export function setupMiddlewares(app: Express): void {
     app.use(bodyParser);
+    app.use(cors);
 }
