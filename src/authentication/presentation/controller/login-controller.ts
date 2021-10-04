d1728f9de965afe594dfd71bf7839f6f95ed2156
Raphael Castro <rafaphcastro@gmail.com>
2021-10-03 23:20:15 -0300

    refactor: move to a sut factory

--- src/authentication/presentation/controller/login-controller.ts
+++ src/authentication/presentation/controller/login-controller.ts
@@ -1,17 +1,31 @@
 import { Controller } from "@app/shared/presentation/protocol/controller";
 import { HttpRequest, HttpResponse } from "@app/shared/presentation/protocol/http";
-import { badRequest } from "@app/shared/presentation/helper/http-helper";
+import { badRequest, serverError } from "@app/shared/presentation/helper/http-helper";
 import { MissingParameterError } from "@app/shared/presentation/error/missing-parameter-error";
 
-interface RequestBody {
+export interface RequestBody {
     email?: string;
     password?: string;
 }
 
-type ResponseBody = Error;
+export type ResponseBody = Error;
 
 export class LoginController implements Controller {
-    public async handle(_request: HttpRequest<RequestBody>): Promise<HttpResponse<ResponseBody>> {
-        return badRequest(new MissingParameterError("email"));
+    public async handle(request: HttpRequest<RequestBody>): Promise<HttpResponse<ResponseBody>> {
+        if (request.body === undefined) {
+            return serverError();
+        }
+
+        if (!request.body.email) {
+            return badRequest(new MissingParameterError("email"));
+        }
+
+        if (!request.body.password) {
+            return badRequest(new MissingParameterError("password"));
+        }
+
+        return {
+            statusCode: 200
+        };
     }
 }
--- test/authentication/presentation/controller/login-controller.spec.ts
+++ test/authentication/presentation/controller/login-controller.spec.ts
@@ -1,18 +1,58 @@
-import { LoginController } from "@app/authentication/presentation/controller/login-controller";
 import faker from "faker";
-import { badRequest } from "@app/shared/presentation/helper/http-helper";
+import { LoginController, RequestBody } from "@app/authentication/presentation/controller/login-controller";
+import { badRequest, serverError } from "@app/shared/presentation/helper/http-helper";
 import { MissingParameterError } from "@app/shared/presentation/error/missing-parameter-error";
+import { ServerError } from "@app/shared/presentation/error/server-error";
+import { HttpRequest } from "@app/shared/presentation/protocol/http";
+
+const makeSut = (): {
+    sut: LoginController;
+} => {
+    const sut = new LoginController();
+    return {
+        sut,
+    };
+};
+
+const makeRequest = (body: RequestBody = {}): HttpRequest<RequestBody> => ({
+    body: {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        ...body,
+    },
+});
 
 describe.only("LoginController", () => {
+    it("Should return a server error if no body is provided", async () => {
+        const { sut } = makeSut();
+        const request = {};
+        const response = await sut.handle(request);
+        const expectedResponse = serverError();
+
+        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
+        expect(response.body).to.be.instanceOf(ServerError);
+    });
+
     it("Should return a bad request if no email is provided", async () => {
-        const sut = new LoginController();
-        const request = {
-            body: {
-                password: faker.internet.password(),
-            },
-        };
+        const { sut } = makeSut();
+        const request = makeRequest({
+            email: undefined,
+        });
         const response = await sut.handle(request);
         const expectedResponse = badRequest(new MissingParameterError("email"));
+
+        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
+        expect(response.body).to.be.instanceOf(MissingParameterError);
+        expect(response.body?.message).to.be.equal(expectedResponse.body?.message);
+    });
+
+    it("Should return a bad request if no password is provided", async () => {
+        const { sut } = makeSut();
+        const request = makeRequest({
+            password: undefined,
+        });
+        const response = await sut.handle(request);
+        const expectedResponse = badRequest(new MissingParameterError("password"));
 
         expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
         expect(response.body).to.be.instanceOf(MissingParameterError);
