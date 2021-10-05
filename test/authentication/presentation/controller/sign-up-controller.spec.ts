885ee487692b96ebbcc839caa235f6bb37d47ca3
Raphael Castro <rafaphcastro@gmail.com>
2021-10-05 00:38:15 -0300

    refactor: move EmailValidation to validator composite

--- test/authentication/presentation/controller/sign-up-controller.spec.ts
+++ test/authentication/presentation/controller/sign-up-controller.spec.ts
@@ -1,24 +1,10 @@
 import faker from "faker";
 import sinon from "sinon";
 import { SignUpController } from "@app/authentication/presentation/controller/sign-up-controller";
-import { ServerError } from "@app/shared/presentation/error/server-error";
-import { InvalidParameterError } from "@app/shared/presentation/error/invalid-parameter-error";
-import { EmailValidator } from "@app/authentication/presentation/protocol/email-validator";
 import { AddAccount } from "@app/authentication/domain/use-case/add-account";
 import { HttpStatusCodes } from "@app/shared/utils/http-status-codes";
 import { Validator } from "@app/shared/presentation/protocol/validator";
 import { badRequest } from "@app/shared/presentation/helper/http-helper";
-
-const makeEmailValidator = (): EmailValidator => {
-    class EmailValidatorStub implements EmailValidator {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        public isValid(_: string): boolean {
-            return true;
-        }
-    }
-
-    return new EmailValidatorStub();
-};
 
 const makeAddAccount = (): AddAccount => {
     class AddAccountStub implements AddAccount {
@@ -47,17 +33,14 @@ const makeValidator = (): Validator => {
 
 const makeSut = (): {
     sut: SignUpController,
-    emailValidatorStub: EmailValidator,
     addAccountStub: AddAccount,
     validatorStub: Validator,
 } => {
-    const emailValidatorStub = makeEmailValidator();
     const addAccountStub = makeAddAccount();
     const validatorStub = makeValidator();
-    const sut = new SignUpController(emailValidatorStub, addAccountStub, validatorStub);
+    const sut = new SignUpController(addAccountStub, validatorStub);
     return {
         sut,
-        emailValidatorStub,
         addAccountStub,
         validatorStub,
     };
@@ -75,48 +58,6 @@ const makeBody = (overrides: Partial<SignUpController.RequestBody> = {}): SignUp
 };
 
 describe("SignUpController", () => {
-    it("Should return a server error response if no body is provided", async () => {
-        const { sut } = makeSut();
-        const response = await sut.handle({});
-
-        expect(response.statusCode).to.be.equal(HttpStatusCodes.INTERNAL_SERVER_ERROR);
-        expect(response.body).to.be.instanceOf(ServerError);
-    });
-
-    it("Should return a bad request if an invalid email is provided", async () => {
-        const { sut, emailValidatorStub } = makeSut();
-        const body = makeBody();
-
-        sinon.stub(emailValidatorStub, "isValid").returns(false);
-        const response = await sut.handle({ body });
-
-        expect(response.statusCode).to.be.equal(HttpStatusCodes.BAD_REQUEST);
-        expect(response.body)
-            .to.be.instanceOf(InvalidParameterError)
-            .that.includes({ paramName: "email" });
-    });
-
-    it("Should call email validator with correct email", async () => {
-        const { sut, emailValidatorStub } = makeSut();
-        const body = makeBody();
-
-        const isValidEmailSpy = sinon.spy(emailValidatorStub, "isValid");
-        await sut.handle({ body });
-
-        expect(isValidEmailSpy.calledOnceWith(body.email as string)).to.be.true;
-    });
-
-    it("Should return a server error if EmailValidator throws a error", async () => {
-        const { sut, emailValidatorStub } = makeSut();
-        const body = makeBody();
-
-        sinon.stub(emailValidatorStub, "isValid").throwsException();
-        const response = await sut.handle({ body });
-
-        expect(response.statusCode).to.be.equal(HttpStatusCodes.INTERNAL_SERVER_ERROR);
-        expect(response.body).to.be.instanceOf(ServerError);
-    });
-
     it("Should call AddAccount with correct values", async () => {
         const { sut, addAccountStub } = makeSut();
         const body = makeBody();
