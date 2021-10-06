4c899a9c19cdafd5e079b5d3feddd6f88c62eb9e
Raphael Castro <rafaphcastro@gmail.com>
2021-10-05 23:08:13 -0300

    test: add Validator to login controller

--- test/authentication/presentation/controller/login-controller.spec.ts
+++ test/authentication/presentation/controller/login-controller.spec.ts
@@ -2,29 +2,27 @@ import sinon from "sinon";
 import faker from "faker";
 import { LoginController } from "@app/authentication/presentation/controller/login-controller";
 import { badRequest, ok, serverError, unauthorized } from "@app/shared/presentation/helper/http-helper";
-import { MissingParameterError } from "@app/shared/presentation/error/missing-parameter-error";
 import { ServerError } from "@app/shared/presentation/error/server-error";
 import { HttpRequest } from "@app/shared/presentation/protocol/http";
-import { EmailValidator } from "@app/authentication/presentation/protocol/email-validator";
-import { InvalidParameterError } from "@app/shared/presentation/error/invalid-parameter-error";
 import { Authenticate } from "@app/authentication/domain/use-case/authenticate";
 import { UnauthorizedError } from "@app/shared/presentation/error/unauthorized-error";
+import { Validator } from "@app/shared/presentation/protocol/validator";
 
-const makeEmailValidator = (): EmailValidator => {
-    class EmailValidatorStub implements EmailValidator {
-        public isValid(): boolean {
-            return true;
+const makeValidator = (): Validator => {
+    class ValidatorStub implements Validator {
+        public async validate(): Promise<Error | undefined> {
+            return undefined;
         }
     }
 
-    return new EmailValidatorStub();
+    return new ValidatorStub();
 };
 
 const FAKE_TOKEN = faker.datatype.uuid();
+
 const makeAuthenticate = (): Authenticate => {
     class AuthenticateStub implements Authenticate {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        public async execute(_input: Authenticate.Input): Promise<Authenticate.Output> {
+        public async execute(): Promise<Authenticate.Output> {
             return FAKE_TOKEN;
         }
     }
@@ -34,15 +32,15 @@ const makeAuthenticate = (): Authenticate => {
 
 const makeSut = (): {
     sut: LoginController;
-    emailValidatorStub: EmailValidator;
+    validatorStub: Validator;
     authenticateStub: Authenticate;
 } => {
-    const emailValidatorStub = makeEmailValidator();
+    const validatorStub = makeValidator();
     const authenticateStub = makeAuthenticate();
-    const sut = new LoginController(emailValidatorStub, authenticateStub);
+    const sut = new LoginController(validatorStub, authenticateStub);
     return {
         sut,
-        emailValidatorStub,
+        validatorStub,
         authenticateStub,
     };
 };
@@ -56,71 +54,36 @@ const makeRequest = (body: LoginController.RequestBody = {}): HttpRequest<LoginC
 });
 
 describe("LoginController", () => {
-    it("Should return a server error if no body is provided", async () => {
-        const { sut } = makeSut();
-        const request = {};
-        const response = await sut.handle(request);
-        const expectedResponse = serverError();
-
-        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
-        expect(response.body).to.be.instanceOf(ServerError);
-    });
-
-    it("Should return a bad request if no email is provided", async () => {
-        const { sut } = makeSut();
-        const request = makeRequest({
-            email: undefined,
-        });
-        const response = await sut.handle(request);
-        const expectedResponse = badRequest(new MissingParameterError("email"));
-
-        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
-        expect(response.body).to.be.instanceOf(MissingParameterError);
-        expect((response.body as MissingParameterError).message).to.be.equal(expectedResponse.body?.message);
-    });
-
-    it("Should return a bad request if no password is provided", async () => {
-        const { sut } = makeSut();
-        const request = makeRequest({
-            password: undefined,
-        });
-        const response = await sut.handle(request);
-        const expectedResponse = badRequest(new MissingParameterError("password"));
-
-        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
-        expect(response.body).to.be.instanceOf(MissingParameterError);
-        expect((response.body as MissingParameterError).message).to.be.equal(expectedResponse.body?.message);
-    });
-
-    it("Should call EmailValidator with correct value", async () => {
-        const { sut, emailValidatorStub } = makeSut();
-        const isValidStub = sinon.stub(emailValidatorStub, "isValid");
-        const email = faker.internet.email();
-        const request = makeRequest({ email });
+    it("Should call Validator with correct value", async () => {
+        const { sut, validatorStub } = makeSut();
+        const validateSpy = sinon.spy(validatorStub, "validate");
+        const request = makeRequest();
 
         await sut.handle(request);
 
-        sinon.assert.calledOnceWithExactly(isValidStub, email);
+        sinon.assert.calledOnceWithExactly(validateSpy, request.body);
     });
 
-    it("Should return a bad request if a invalid email is provided", async () => {
-        const { sut, emailValidatorStub } = makeSut();
+    it("Should returns a bad request response if Validator fails", async () => {
+        const { sut, validatorStub } = makeSut();
+        const error = new Error();
+        sinon.stub(validatorStub, "validate").resolves(error);
         const request = makeRequest();
-        sinon.stub(emailValidatorStub, "isValid").returns(false);
-        const response = await sut.handle(request);
-        const expectedResponse = badRequest(new InvalidParameterError("email"));
 
-        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
-        expect(response.body).to.be.instanceOf(InvalidParameterError);
-        expect((response.body as MissingParameterError).message).to.be.equal(expectedResponse.body?.message);
+        const response = await sut.handle(request);
+        const expectedResponse = badRequest(error);
+
+        expect(response).to.be.deep.equal(expectedResponse);
     });
 
-    it("Should return a server error if email validator throws", async () => {
-        const { sut, emailValidatorStub } = makeSut();
-        sinon.stub(emailValidatorStub, "isValid").throwsException();
+    it("Should return a server error response if Validator throws", async () => {
+        const { sut, validatorStub } = makeSut();
+        const error = new Error();
+        sinon.stub(validatorStub, "validate").rejects(error);
         const request = makeRequest();
+
         const response = await sut.handle(request);
-        const expectedResponse = serverError();
+        const expectedResponse = serverError(error);
 
         expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
         expect(response.body).to.be.instanceOf(ServerError);
@@ -128,15 +91,28 @@ describe("LoginController", () => {
 
     it("Should call Authenticate with correct values", async () => {
         const { sut, authenticateStub } = makeSut();
-        const executeStub = sinon.stub(authenticateStub, "execute");
-        const email = faker.internet.email();
-        const password = faker.internet.password();
-        const body = { email, password };
+        const executeSpy = sinon.spy(authenticateStub, "execute");
+        const body = {
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        };
         const request = makeRequest(body);
 
         await sut.handle(request);
 
-        sinon.assert.calledOnceWithExactly(executeStub, body);
+        sinon.assert.calledOnceWithExactly(executeSpy, body);
+    });
+
+    it("Should return a server error response if Authenticate throws", async () => {
+        const { sut, authenticateStub } = makeSut();
+        sinon.stub(authenticateStub, "execute").rejects();
+        const request = makeRequest();
+
+        const response = await sut.handle(request);
+        const expectedResponse = serverError();
+
+        expect(response.statusCode).to.be.deep.equal(expectedResponse.statusCode);
+        expect(response.body).to.be.instanceOf(ServerError);
     });
 
     it("Should return a unauthorized response if invalid credentials are provided", async () => {
@@ -148,17 +124,6 @@ describe("LoginController", () => {
 
         expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
         expect(response.body).to.be.instanceOf(UnauthorizedError);
-    });
-
-    it("Should return a server error response if Authenticate throws", async () => {
-        const { sut, authenticateStub } = makeSut();
-        sinon.stub(authenticateStub, "execute").throwsException();
-        const request = makeRequest();
-        const response = await sut.handle(request);
-        const expectedResponse = serverError();
-
-        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
-        expect(response.body).to.be.instanceOf(ServerError);
     });
 
     it("Should return a ok response if valid credentials are provided", async () => {
