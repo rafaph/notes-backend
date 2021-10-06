f29f7f1567c6b1559739ff18761bc2fa2b5b597c
Raphael Castro <rafaphcastro@gmail.com>
2021-10-06 00:15:52 -0300

    feat: ensure DatabaseAuthentication calls LoadAccountByEmailRepository

---
+++ src/authentication/data/protocol/load-account-by-email-repository.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from "@app/authentication/domain/model/account-model";
+
+export interface LoadAccountByEmailRepository {
+    execute(input: LoadAccountByEmailRepository.Input): Promise<LoadAccountByEmailRepository.Output>;
+}
+
+export namespace LoadAccountByEmailRepository {
+    export interface Input {
+        email: string;
+    }
+    export type Output = AccountModel;
+}
