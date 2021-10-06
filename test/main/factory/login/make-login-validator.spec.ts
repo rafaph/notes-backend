8bcbb70bd78efefdfc9ec5302f42aed668fea5a5
Raphael Castro <rafaphcastro@gmail.com>
2021-10-05 23:36:51 -0300

    test: add login validator factory

--- test/main/factory/login/make-login-validator.spec.ts
+++ test/main/factory/login/make-login-validator.spec.ts
@@ -1,13 +1,12 @@
 import sinon from "sinon";
-import { makeSignUpValidator } from "@app/main/factory/make-sign-up-validator";
 import * as compositeModule from "@app/shared/presentation/validator/composite-validator";
 import { RequiredFieldValidator } from "@app/shared/presentation/validator/required-field-validator";
-import { AreFieldsDifferentValidator } from "@app/shared/presentation/validator/are-fields-different-validator";
 import { IsEmailValidator } from "@app/shared/presentation/validator/is-email-validator";
 import { EmailValidatorAdapter } from "@app/authentication/utils/email-validator-adapter";
+import { makeLoginValidator } from "@app/main/factory/login/make-login-validator";
 
 
-describe("makeSignUpValidator", () => {
+describe("makeLoginValidator", () => {
     afterEach(() => {
         sinon.restore();
     });
@@ -16,16 +15,15 @@ describe("makeSignUpValidator", () => {
         const compositeValidatorStub = sinon.createStubInstance(compositeModule.CompositeValidator);
         const constructorStub = sinon.stub(compositeModule, "CompositeValidator").onFirstCall().returns(compositeValidatorStub);
 
-        makeSignUpValidator();
+        makeLoginValidator();
 
         const validators = [];
 
-        const requiredFields = ["name", "email", "password", "passwordConfirmation"];
+        const requiredFields = ["email", "password"];
         for (const field of requiredFields) {
             validators.push(new RequiredFieldValidator(field));
         }
 
-        validators.push(new AreFieldsDifferentValidator("password", "passwordConfirmation"));
         validators.push(new IsEmailValidator("email", new EmailValidatorAdapter()));
 
         sinon.assert.calledOnceWithExactly(constructorStub, validators);
