9b649fdb70df34cfab9cc8e0b4d4f5320f0d0852
Raphael Castro <rafaphcastro@gmail.com>
2021-10-06 21:21:18 -0300

    test: add verify to hasher argon2

--- test/authentication/infrastructure/hashing/hasher-argon2-adapter.spec.ts
+++ test/authentication/infrastructure/hashing/hasher-argon2-adapter.spec.ts
@@ -14,15 +14,23 @@ describe("HasherArgon2Adapter", () => {
         options?: (argon2.Options & { raw?: false | undefined; }) | undefined
     ], Promise<string>>;
 
+    let verifyStub: sinon.SinonStub<[
+        hash: string,
+        plain: Buffer | string,
+        options?: argon2.Options | undefined
+    ], Promise<boolean>>;
+
     beforeEach(() => {
         hashStub = sinon.stub(argon2, "hash").onFirstCall().resolves(FAKE_HASHED_VALUE);
+        verifyStub = sinon.stub(argon2, "verify").onFirstCall().resolves(true);
     });
 
     afterEach(() => {
         hashStub.restore();
+        verifyStub.restore();
     });
 
-    it("Should call argon2 with correct values", async () => {
+    it("Should call argon2 hash with correct values", async () => {
         const sut = makeSut();
         const password = faker.internet.password();
 
@@ -35,6 +43,21 @@ describe("HasherArgon2Adapter", () => {
         );
     });
 
+    it("Should call argon2 verify with correct values", async () => {
+        const sut = makeSut();
+        const hash = faker.internet.password();
+        const plain = faker.internet.password();
+
+        await sut.verify(hash, plain);
+
+        sinon.assert.calledOnceWithExactly(
+            verifyStub,
+            hash,
+            plain,
+            { type: ARGON2_TYPE },
+        );
+    });
+
     it("Should return a hash on success", async () => {
         const sut = makeSut();
         await expect(
@@ -42,12 +65,31 @@ describe("HasherArgon2Adapter", () => {
         ).to.eventually.be.equal(FAKE_HASHED_VALUE);
     });
 
-    it("Should throw if argon2 throws", async () => {
+    it("Should return true for verify on success", async () => {
+        const sut = makeSut();
+        const hash = faker.internet.password();
+        const plain = faker.internet.password();
+
+        const result = await sut.verify(hash, plain);
+
+        expect(result).to.be.true;
+    });
+
+    it("Should throw if argon2 hash throws", async () => {
         hashStub.onFirstCall().rejects();
         const sut = makeSut();
 
         await expect(
             sut.hash(faker.internet.password()),
+        ).to.eventually.be.rejected;
+    });
+
+    it("Should throw if argon2 verify throws", async () => {
+        verifyStub.onFirstCall().rejects();
+        const sut = makeSut();
+
+        await expect(
+            sut.verify(faker.internet.password(), faker.internet.password()),
         ).to.eventually.be.rejected;
     });
 });
