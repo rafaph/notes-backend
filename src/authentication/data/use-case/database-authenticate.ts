094514e8cca3d08c19f073fa48a3f231e0e3c2b3
Raphael Castro <rafaphcastro@gmail.com>
2021-10-06 21:07:52 -0300

    feat: ensure Hasher is called with correct values

--- src/authentication/data/use-case/database-authenticate.ts
+++ src/authentication/data/use-case/database-authenticate.ts
@@ -1,14 +1,24 @@
 import { Authenticate } from "@app/authentication/domain/use-case/authenticate";
 import { LoadAccountByEmailRepository } from "@app/authentication/data/protocol/persistence/load-account-by-email-repository";
+import { Hasher } from "@app/authentication/data/protocol/cryptography/hasher";
 
 export class DatabaseAuthenticate implements Authenticate {
     public constructor(
         private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hasher: Hasher,
     ) {
     }
 
-    public async execute({ email }: Authenticate.Input): Promise<Authenticate.Output> {
-        await this.loadAccountByEmailRepository.execute({ email });
+    public async execute({ email, password }: Authenticate.Input): Promise<Authenticate.Output> {
+        const account = await this.loadAccountByEmailRepository.execute({ email });
+
+        if (!account) {
+            return undefined;
+        }
+
+        await this.hasher.verify(account.password, password);
+
         return undefined;
+
     }
 }
