9db96ccd45b2481d4defc87def9ecfd4a0ac07a1
Raphael Castro <rafaphcastro@gmail.com>
2021-10-07 22:43:45 -0300

    feat: ensure SequelizeAccountRepository returns an Account on loadByEmail success

--- src/authentication/infrastructure/database/sequelize/sequelize-account-repository.ts
+++ src/authentication/infrastructure/database/sequelize/sequelize-account-repository.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 import { AddAccountRepository } from "@app/authentication/data/protocol/persistence/add-account-repository";
 import { AccountFactory, SequelizeAccount } from "@app/authentication/infrastructure/database/sequelize/model/account";
+import { LoadAccountByEmailRepository } from "@app/authentication/data/protocol/persistence/load-account-by-email-repository";
 
-export class SequelizeAccountRepository implements AddAccountRepository {
+export class SequelizeAccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
     private readonly account: SequelizeAccount;
 
     public constructor(sequelize: Sequelize) {
@@ -11,5 +12,19 @@ export class SequelizeAccountRepository implements AddAccountRepository {
 
     public add(input: AddAccountRepository.Input): Promise<AddAccountRepository.Output> {
         return this.account.create(input);
+    }
+
+    public async loadByEmail({ email }: LoadAccountByEmailRepository.Input): Promise<LoadAccountByEmailRepository.Output> {
+        const account = await this.account.findOne({
+            where: {
+                email,
+            },
+        });
+
+        if (account) {
+            return account;
+        }
+
+        return undefined;
     }
 }
