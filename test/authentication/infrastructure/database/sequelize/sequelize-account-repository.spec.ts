4cd334cc9b8f27b82c9e81b34439bdaee66043a0
Raphael Castro <rafaphcastro@gmail.com>
2021-10-07 22:43:59 -0300

    test: ensure SequelizeAccountRepository returns an Account on loadByEmail success

--- test/authentication/infrastructure/database/sequelize/sequelize-account-repository.spec.ts
+++ test/authentication/infrastructure/database/sequelize/sequelize-account-repository.spec.ts
@@ -1,30 +1,29 @@
 import { Sequelize } from "sequelize";
 import faker from "faker";
 import { SequelizeAccountRepository } from "@app/authentication/infrastructure/database/sequelize/sequelize-account-repository";
-import { AddAccountRepository } from "@app/authentication/data/protocol/persistence/add-account-repository";
 import { SequelizeClient } from "@app/shared/infrastructure/sequelize-client";
+import { AccountFactory, SequelizeAccount } from "@app/authentication/infrastructure/database/sequelize/model/account";
 
 const makeSut = (sequelize: Sequelize): SequelizeAccountRepository => (
     new SequelizeAccountRepository(sequelize)
 );
 
-const makeInput = (input: Partial<AddAccountRepository.Input> = {}): AddAccountRepository.Input => ({
-    name: faker.name.firstName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    ...input,
-});
-
-describe("SequelizeAddAccountRepository", () => {
+describe.only("SequelizeAccountRepository", () => {
     let sequelize: Sequelize;
+    let sequelizeAccount: SequelizeAccount;
 
-    before(() => {
+    before(async () => {
         sequelize = SequelizeClient.getClient();
+        sequelizeAccount = AccountFactory(sequelize);
     });
 
     it("Should return an account on success", async () => {
         const sut = makeSut(sequelize);
-        const input = makeInput();
+        const input = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        };
 
         const account = await sut.add(input);
 
@@ -32,5 +31,25 @@ describe("SequelizeAddAccountRepository", () => {
         expect(account.name).to.be.equal(input.name);
         expect(account.email).to.be.equal(input.email);
         expect(account.password).to.be.equal(input.password);
+    });
+
+    it("Should return a account on loadByEmail success", async () => {
+        const createdAccount = await sequelizeAccount.create({
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        });
+
+        const sut = makeSut(sequelize);
+
+        const account = await sut.loadByEmail({
+            email: createdAccount.email,
+        });
+
+        expect(account).to.not.be.undefined;
+        expect(account?.id).to.be.equal(createdAccount.id);
+        expect(account?.name).to.be.equal(createdAccount.name);
+        expect(account?.email).to.be.equal(createdAccount.email);
+        expect(account?.password).to.be.equal(createdAccount.password);
     });
 });
