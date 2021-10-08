d05f22d38b93dddeb34421abe565d93bb8f21395
Raphael Castro <rafaphcastro@gmail.com>
2021-10-07 22:56:21 -0300

    refactor: rename LoadAccountByEmailRepository param

--- src/authentication/data/protocol/persistence/load-account-by-email-repository.ts
+++ src/authentication/data/protocol/persistence/load-account-by-email-repository.ts
@@ -1,12 +1,10 @@
 import { AccountModel } from "@app/authentication/domain/model/account-model";
 
 export interface LoadAccountByEmailRepository {
-    loadByEmail(input: LoadAccountByEmailRepository.Input): Promise<LoadAccountByEmailRepository.Output>;
+    loadByEmail(email: LoadAccountByEmailRepository.Input): Promise<LoadAccountByEmailRepository.Output>;
 }
 
 export namespace LoadAccountByEmailRepository {
-    export interface Input {
-        email: string;
-    }
+    export type Input = string;
     export type Output = AccountModel | undefined;
 }
