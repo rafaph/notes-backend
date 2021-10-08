4ecff8c69791afbb194b9471a5296aecfe9deab8
Raphael Castro <rafaphcastro@gmail.com>
2021-10-08 10:29:32 -0300

    test: ensure login on success

--- test/main/route/login.spec.ts
+++ test/main/route/login.spec.ts
@@ -2,26 +2,37 @@ import request from "supertest";
 import faker from "faker";
 import { Express } from "express";
 import { TestApplication } from "@test/helper/test-application";
+import { SequelizeAccountRepository } from "@app/authentication/infrastructure/database/sequelize/sequelize-account-repository";
+import { Sequelize } from "sequelize";
+import { SequelizeClient } from "@app/shared/infrastructure/sequelize-client";
+import { Argon2Adapter } from "@app/authentication/infrastructure/cryptography/argon2-adapter";
 
 
-describe("@integration SignUp Routes", () => {
+describe("@integration Login Routes", () => {
     let app: Express;
+    let sequelize: Sequelize;
 
     before(() => {
         app = TestApplication.app;
+        sequelize = SequelizeClient.getClient();
     });
 
-    it("Should return an account on success", async () => {
-        const route = "/api/sign-up";
+    it("Should login on success", async () => {
+        const route = "/api/login";
+        const email = faker.internet.email();
         const password = faker.internet.password();
+        const argon2 = new Argon2Adapter();
+        await new SequelizeAccountRepository(sequelize).add({
+            name: faker.name.firstName(),
+            email,
+            password: await argon2.hash(password),
+        });
 
         await request(app)
             .post(route)
             .send({
-                name: faker.name.firstName(),
-                email: faker.internet.email(),
+                email,
                 password,
-                passwordConfirmation: password,
             })
             .expect(200);
     });
