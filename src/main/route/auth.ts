cfd36101bc1e6a9648916a4eb95714c7fab899e2
Raphael Castro <rafaphcastro@gmail.com>
2021-10-08 22:25:43 -0300

    refactor: improve routes

--- src/main/route/auth.ts
+++ src/main/route/auth.ts
@@ -1,8 +1,10 @@
 import { IRouter } from "express";
 import { ExpressRouteAdapter } from "@app/main/adapter/express/express-route-adapter";
+import { makeLoginController } from "@app/main/factory/login/make-login-controller";
 import { makeSignUpController } from "@app/main/factory/sign-up/make-sign-up-controller";
 
 
 export function route(router: IRouter): void {
+    router.post("/login", ExpressRouteAdapter.adapt(makeLoginController()));
     router.post("/sign-up", ExpressRouteAdapter.adapt(makeSignUpController()));
 }
--- test/main/route/auth/login.spec.ts
+++ test/main/route/auth/login.spec.ts
@@ -1,40 +1,37 @@
 import request from "supertest";
 import faker from "faker";
 import { Express } from "express";
+import argon2 from "argon2";
+import { Sequelize } from "sequelize";
 import { TestApplication } from "@test/helper/test-application";
 import { SequelizeAccountRepository } from "@app/authentication/infrastructure/database/sequelize/account/sequelize-account-repository";
-import { Sequelize } from "sequelize";
 import { SequelizeClient } from "@app/shared/infrastructure/sequelize-client";
-import { Argon2Adapter } from "@app/authentication/infrastructure/cryptography/argon2-adapter";
+import { HttpStatusCodes } from "@app/shared/utils/http-status-codes";
 
-
-describe("@integration Login Routes", () => {
+describe("@integration POST /api/login", () => {
+    const route = "/api/login";
     let app: Express;
     let sequelize: Sequelize;
-    let argon2: Argon2Adapter;
 
     before(() => {
         app = TestApplication.app;
         sequelize = SequelizeClient.getClient();
-        argon2 = new Argon2Adapter();
     });
 
-    it("Should login on success", async () => {
-        const route = "/api/login";
+    it("Should returns a ok response on login success", async () => {
         const email = faker.internet.email();
         const password = faker.internet.password();
         await new SequelizeAccountRepository(sequelize).add({
             name: faker.name.firstName(),
             email,
-            password: await argon2.hash(password),
+            password: await argon2.hash(password, {
+                type: argon2.argon2id,
+            }),
         });
 
         await request(app)
             .post(route)
-            .send({
-                email,
-                password,
-            })
-            .expect(200);
+            .send({ email, password })
+            .expect(HttpStatusCodes.OK);
     });
 });
