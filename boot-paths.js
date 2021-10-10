db48329250f9c239a36c4820d2bc1c5b0dcdf668
Raphael Castro <rafaphcastro@gmail.com>
2021-10-09 23:38:16 -0300

    refactor: change setup-routes to use fs module instead of glob

---
+++ boot-paths.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+const { compilerOptions } = require('./tsconfig.json');
+const tsConfigPaths = require('tsconfig-paths');
+
+tsConfigPaths.register({
+    baseUrl: compilerOptions.outDir,
+    paths: compilerOptions.paths,
+});
--- src/main/config/setup-routes.ts
+++ src/main/config/setup-routes.ts
@@ -1,14 +1,16 @@
+import fs from "fs";
+import path from "path";
 import { Express, Router } from "express";
-import glob from "glob";
 
-const files = glob.sync("**/src/main/route/*.ts");
+const files = fs
+    .readdirSync(path.join(__dirname, "..", "route"))
+    .filter(file => !file.includes(".d.ts"));
 
 export async function setupRoutes(app: Express): Promise<void> {
     const router = Router();
 
     for (const file of files) {
-        const { route } = await import(`@app${file.slice(3)}`);
-
+        const { route } = await import(`../route/${file}`);
         route(router);
     }
 
