4a17bf64f4639da7f8995a72026fd7e4706c4d1f
Raphael Castro <rafaphcastro@gmail.com>
2021-10-10 19:38:08 -0300

    chore: add config for debug

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ down:
 	docker-compose down --remove-orphans -t 30
 
 shell:
-	docker-compose run -p 127.0.0.1:3000:3000 server bash
+	docker-compose run -p 3000:3000 -p 9229:9229 server bash
 	$(MAKE) down
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,11 @@
+import { SequelizeClient } from "@app/shared/infrastructure/sequelize-client";
 import { createApp } from "@app/main/config/create-app";
 import { env } from "@app/main/config/env";
+
+async function onExit(): Promise<void> {
+    const sequelize = SequelizeClient.getClient();
+    await sequelize.close();
+}
 
 (async (): Promise<void> => {
     const app = await createApp();
@@ -9,3 +15,7 @@ import { env } from "@app/main/config/env";
         console.log(`Server running at http://localhost:${env.PORT}`);
     });
 })();
+
+["exit", "SIGINT", "SIGUSR1", "SIGUSR2", "uncaughtException", "SIGTERM"].forEach((eventType) => {
+    process.on(eventType, onExit);
+});
