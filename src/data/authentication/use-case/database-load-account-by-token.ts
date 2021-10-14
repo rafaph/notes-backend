78dcb13d04bfc8243dfa80d14a0cb11eae156dbd
Raphael Castro <rafaphcastro@gmail.com>
2021-10-13 23:38:38 -0300

    feat: ensure DatabaseLoadAccountByToken calls LoadAccountByTokenRepository with correct value

--- src/data/authentication/use-case/database-load-account-by-token.ts
+++ src/data/authentication/use-case/database-load-account-by-token.ts
@@ -1,14 +1,17 @@
 import { LoadAccountByToken } from "@app/domain/authentication/use-case/load-account-by-token";
 import { Decrypter } from "@app/data/authentication/protocol/cryptography/decrypter";
+import { LoadAccountByTokenRepository } from "@app/data/authentication/protocol/persistence/load-account-by-token-repository";
 
 export class DatabaseLoadAccountByToken implements LoadAccountByToken {
     public constructor(
         private readonly decrypter: Decrypter,
+        private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
     ) {
     }
 
     public async execute(input: LoadAccountByToken.Input): Promise<LoadAccountByToken.Output> {
         await this.decrypter.decrypt(input.accessToken);
+        await this.loadAccountByTokenRepository.loadByToken(input.accessToken);
         return undefined;
     }
 }
