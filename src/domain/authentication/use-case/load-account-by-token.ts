ef9ca72a19db232d23da56a0a3a1b74348f7e8e4
Raphael Castro <rafaphcastro@gmail.com>
2021-10-13 21:42:02 -0300

    feat: ensure AuthMiddleware returns noContent if Authorization header has a valid token

---
+++ src/domain/authentication/use-case/load-account-by-token.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from "@app/domain/authentication/model/account-model";
+
+export interface LoadAccountByToken {
+    execute(input: LoadAccountByToken.Input): Promise<LoadAccountByToken.Output>;
+}
+
+export namespace LoadAccountByToken {
+    export interface Input {
+        accessToken: string;
+    }
+
+    export type Output = AccountModel | undefined;
+}
--- src/presentation/authentication/middleware/auth-middleware.ts
+++ src/presentation/authentication/middleware/auth-middleware.ts
@@ -1,11 +1,49 @@
 import { Middleware } from "@app/presentation/shared/protocol/middleware";
 import { HttpRequest, HttpResponse } from "@app/presentation/shared/protocol/http";
-import { forbidden } from "@app/presentation/shared/helper/http/http-helper";
+import { badRequest, forbidden, noContent, serverError } from "@app/presentation/shared/helper/http/http-helper";
 import { AccessDeniedError } from "@app/presentation/shared/error/access-denied-error";
+import { Validator } from "@app/presentation/shared/protocol/validator";
+import { LoadAccountByToken } from "@app/domain/authentication/use-case/load-account-by-token";
+
+interface AuthorizationHeader {
+    Authorization?: string;
+}
 
 export class AuthMiddleware implements Middleware {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public async handle(_request: HttpRequest): Promise<HttpResponse> {
-        return forbidden(new AccessDeniedError());
+    public constructor(
+        private readonly validator: Validator,
+        private readonly loadAccountByToken: LoadAccountByToken,
+    ) {
     }
+
+    public async handle(request: HttpRequest<unknown, AuthorizationHeader>): Promise<HttpResponse> {
+        try {
+            const error = await this.validator.validate(request.headers);
+            if (error) {
+                return badRequest(error);
+            }
+
+            const headers = request.headers as Required<AuthMiddleware.AuthorizationHeader>;
+
+            const account = await this.loadAccountByToken.execute({
+                accessToken: headers.Authorization.substring(7)
+            });
+
+            if(!account) {
+                return forbidden(new AccessDeniedError());
+            }
+
+            return noContent();
+        } catch (error) {
+            return serverError(error as Error);
+        }
+    }
+}
+
+export namespace AuthMiddleware {
+    export interface AuthorizationHeader {
+        Authorization?: string;
+    }
+
+    export type ResponseBody = Error | undefined;
 }
