bf650de89f0d8054c5163158727af351c3cf2d7d
Raphael Castro <rafaphcastro@gmail.com>
2021-10-13 23:38:49 -0300

    test: ensure DatabaseLoadAccountByToken calls LoadAccountByTokenRepository with correct value

--- test/data/authentication/use-case/database-load-account-by-token.spec.ts
+++ test/data/authentication/use-case/database-load-account-by-token.spec.ts
@@ -3,6 +3,7 @@ import sinon from "sinon";
 import { DatabaseLoadAccountByToken } from "@app/data/authentication/use-case/database-load-account-by-token";
 import { LoadAccountByToken } from "@app/domain/authentication/use-case/load-account-by-token";
 import { Decrypter } from "@app/data/authentication/protocol/cryptography/decrypter";
+import { LoadAccountByTokenRepository } from "@app/data/authentication/protocol/persistence/load-account-by-token-repository";
 
 const FAKE_ACCOUNT_ID = faker.datatype.uuid();
 const makeDecrypterStub = (): Decrypter => {
@@ -17,6 +18,21 @@ const makeDecrypterStub = (): Decrypter => {
     return new DecrypterStub();
 };
 
+const makeLoadAccountByTokenRepository = (): LoadAccountByTokenRepository => {
+    class LoadAccountByTokenRepositoryStub implements LoadAccountByTokenRepository {
+        public async loadByToken(): Promise<LoadAccountByTokenRepository.Output> {
+            return {
+                id: FAKE_ACCOUNT_ID,
+                name: faker.name.firstName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+            };
+        }
+    }
+
+    return new LoadAccountByTokenRepositoryStub();
+};
+
 const makeInput = (accessToken = faker.datatype.uuid()): LoadAccountByToken.Input => {
     return {
         accessToken,
@@ -26,11 +42,14 @@ const makeInput = (accessToken = faker.datatype.uuid()): LoadAccountByToken.Inpu
 const makeSut = (): {
     sut: DatabaseLoadAccountByToken;
     decrypterStub: Decrypter;
+    loadAccountByTokenRepositoryStub: LoadAccountByTokenRepository;
 } => {
     const decrypterStub = makeDecrypterStub();
+    const loadAccountByTokenRepositoryStub = makeLoadAccountByTokenRepository();
     return {
-        sut: new DatabaseLoadAccountByToken(decrypterStub),
+        sut: new DatabaseLoadAccountByToken(decrypterStub, loadAccountByTokenRepositoryStub),
         decrypterStub,
+        loadAccountByTokenRepositoryStub
     };
 };
 
@@ -59,5 +78,15 @@ describe("DatabaseLoadAccountByToken", () => {
 
         const input = makeInput();
         await expect(sut.execute(input)).to.eventually.be.undefined;
+    });
+
+    it("Should call LoadAccountByTokenRepository with correct value", async () => {
+        const { sut, loadAccountByTokenRepositoryStub } = makeSut();
+        const loadByTokenSpy = sinon.spy(loadAccountByTokenRepositoryStub, "loadByToken");
+
+        const input = makeInput();
+        await sut.execute(input);
+
+        sinon.assert.calledOnceWithExactly(loadByTokenSpy, input.accessToken);
     });
 });
