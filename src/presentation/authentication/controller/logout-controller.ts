1732a63af05a255520ee5b7abef625b8515db5c2
Raphael Castro <rafaphcastro@gmail.com>
2021-10-15 22:39:11 -0300

    feat: ensure LogoutController returns a internal server error response if Deauthenticate throws

--- src/presentation/authentication/controller/logout-controller.ts
+++ src/presentation/authentication/controller/logout-controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@app/presentation/shared/protocol/controller";
 import { HttpRequest, HttpResponse } from "@app/presentation/shared/protocol/http";
 import { Deauthenticate } from "@app/domain/authentication/use-case/deauthenticate";
+import { serverError } from "@app/presentation/shared/helper/http/http-helper";
 
 export class LogoutController implements Controller {
     public constructor(
@@ -9,10 +10,15 @@ export class LogoutController implements Controller {
     }
 
     public async handle(request: HttpRequest<unknown, unknown, unknown, { accountId: string }>): Promise<HttpResponse> {
-        const id = request.data?.accountId as string;
-        await this.deauthenticate.execute({ id });
-        return {
-            statusCode: 200,
-        };
+        try {
+            const id = request.data?.accountId as string;
+            await this.deauthenticate.execute({ id });
+
+            return {
+                statusCode: 200
+            };
+        } catch (error) {
+            return serverError(error as Error);
+        }
     }
 }
