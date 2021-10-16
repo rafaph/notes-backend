e15996dd656f69f387bd34f39bc045374f131f46
Raphael Castro <rafaphcastro@gmail.com>
2021-10-15 23:21:00 -0300

    test: ensure LogoutController return a unauthorized response if Deauthenticate returns false

--- test/presentation/authentication/controller/logout-controller.spec.ts
+++ test/presentation/authentication/controller/logout-controller.spec.ts
@@ -2,12 +2,12 @@ import sinon from "sinon";
 import faker from "faker";
 import { LogoutController } from "@app/presentation/authentication/controller/logout-controller";
 import { Deauthenticate } from "@app/domain/authentication/use-case/deauthenticate";
-import { noContent, serverError } from "@app/presentation/shared/helper/http/http-helper";
+import { noContent, serverError, unauthorized } from "@app/presentation/shared/helper/http/http-helper";
 
 const makeDeauthenticate = (): Deauthenticate => {
     class DeauthenticateStub implements Deauthenticate {
         public async execute(): Promise<Deauthenticate.Output> {
-            return undefined;
+            return true;
         }
     }
 
@@ -38,13 +38,24 @@ describe("LogoutController", () => {
         sinon.assert.calledOnceWithExactly(executeSpy, { id: accountId });
     });
 
-    it("Should returns a internal server response if Deauthenticate throws", async () => {
+    it("Should return a internal server response if Deauthenticate throws", async () => {
         const { sut, deauthenticateStub } = makeSut();
         const error = new Error();
         sinon.stub(deauthenticateStub, "execute").rejects(error);
 
         const response = await sut.handle({});
         const expectedResponse = serverError(error);
+
+        expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
+        expect(response.body).to.be.instanceOf(expectedResponse.body?.constructor);
+    });
+
+    it("Should return a unauthorized response if Deauthenticate returns false", async () => {
+        const { sut, deauthenticateStub } = makeSut();
+        sinon.stub(deauthenticateStub, "execute").resolves(false);
+
+        const response = await sut.handle({});
+        const expectedResponse = unauthorized();
 
         expect(response.statusCode).to.be.equal(expectedResponse.statusCode);
         expect(response.body).to.be.instanceOf(expectedResponse.body?.constructor);
