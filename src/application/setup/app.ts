945beca984553d797430e64449efc806b19b0d3c
Raphael Castro <rafaphcastro@gmail.com>
2021-10-19 21:34:16 -0300

    chore: add swagger api docs

--- src/application/setup/app.ts
+++ src/application/setup/app.ts
@@ -1,9 +1,13 @@
 import express, { json, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import responseTime from "response-time";
 import { SERVICE_UNAVAILABLE } from "http-status";
 import { container } from "tsyringe";
-import { PORT, PROD, SHUTDOWN_TIMEOUT } from "@app/domains/common/utils/environment";
+import { ENV, PORT, PROD, SHUTDOWN_TIMEOUT } from "@app/domains/common/utils/environment";
 import { contentType } from "@app/application/middlewares/content-type";
 import { ResponseError } from "@app/domains/common/utils/response-error";
 import { Logger } from "@app/domains/common/utils/logger";
@@ -18,6 +22,14 @@ export class App {
         this.app = express();
         this.running = !PROD;
 
+        if (ENV !== "test" && !PROD) {
+            this.app.use(morgan("dev"));
+            const swaggerDocument = YAML.load("doc/api-reference.yml");
+            this.app.use("/api-docs", swaggerUi.serve);
+            this.app.get("/api-docs", swaggerUi.setup(swaggerDocument));
+        }
+
+        this.app.use(helmet());
         this.app.use(responseTime());
         this.app.use(this.checkServerStatus);
         this.app.use(contentType);
