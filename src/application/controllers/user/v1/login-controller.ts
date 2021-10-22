cb17613f6fbb8109182e9eb6c688c8c0b0682a21
Raphael Castro <rafaphcastro@gmail.com>
2021-10-21 23:45:30 -0300

    feat: add login controller

---
+++ src/application/controllers/user/v1/login-controller.ts
@@ -0,0 +1,43 @@
+import { inject, Lifecycle, registry, scoped } from "tsyringe";
+import { OK } from "http-status";
+import Joi from "joi";
+import { Controller, Request, Response } from "@app/domains/common/interfaces/controller";
+import { IAuthenticateAuthenticationService } from "@app/domains/user/interfaces/in/authentication-service";
+import { requestValidator } from "@app/application/middlewares/request-validator";
+
+namespace LoginController {
+    export interface Request {
+        email: string;
+        password: string;
+    }
+
+    export type Response = {
+        access_token: string;
+    };
+}
+
+@scoped(Lifecycle.ResolutionScoped)
+@registry([{ token: "Controller", useClass: LoginController }])
+export class LoginController extends Controller<LoginController.Request, LoginController.Response> {
+    private readonly schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    });
+
+    public middlewares = [requestValidator(this.schema, "body")];
+
+    public constructor(
+        @inject("AuthenticationService") private readonly authenticationService: IAuthenticateAuthenticationService,
+    ) {
+        super("post", "/api/v1/login");
+    }
+
+    public async handle(req: Request<LoginController.Request>, res: Response<LoginController.Response>): Promise<void> {
+        const { email, password } = req.body;
+        const accessToken = await this.authenticationService.authenticate(email, password);
+
+        res.status(OK).json({
+            access_token: accessToken,
+        });
+    }
+}
